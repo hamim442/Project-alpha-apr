01e047c0940b1c643916ad0c42ce87e413cd7e80
Al Hamim <hamim442@icloud.com>
2024-05-30 15:32:45 -0700

    feature 10 complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,34 @@
-from accounts.forms import LoginForm
+from accounts.forms import LoginForm, SignupForm
 from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+
+def user_signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            password_confirmation = form.cleaned_data["password_confirmation"]
+
+            if password == password_confirmation:
+                user = User.objects.create_user(
+                    username=username,
+                    password=password,
+                )
+
+                login(request, user)
+                return redirect("list_projects")
+            else:
+                form.add_error("password", "Passwords do not match")
+
+    else:
+        form = SignupForm()
+    context = {"form": form}
+    return render(request, "accounts/signup.html", context)
 
 
 def user_login(request):
