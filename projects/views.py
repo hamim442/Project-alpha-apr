ea578be69c961aebf80c8e938d5b3ba6ca635e4b
Al Hamim <hamim442@icloud.com>
2024-05-30 12:52:36 -0700

    Feature 5 complete

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from projects.models import Project
 
 # Create your views here.
+
+
+def list_view(request):
+    project = Project.objects.all()
+    context = {"list_view": project}
+    return render(request, "projects/list.html", context)
