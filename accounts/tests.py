a27000fe30e3297a702b1b44dcc5d448b8033175
Al Hamim <hamim442@icloud.com>
2024-05-31 15:30:56 -0700

    Final submission

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,19 +1,19 @@
-from django.test import TestCase
-from accounts.forms import LoginForm
-from django.contrib.auth import login, authenticate, logout
-from django.shortcuts import render, redirect
+# from django.test import TestCase
+# from accounts.forms import LoginForm
+# from django.contrib.auth import login, authenticate, logout
+# from django.shortcuts import render, redirect
 
-# Create your tests here.
+# # Create your tests here.
 
 
-def user_login(request):
-    if request.method == "POST":
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data["username"]
-            password = form.cleaned_data["password"]
+# def user_login(request):
+#     if request.method == "POST":
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data["username"]
+#             password = form.cleaned_data["password"]
 
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect("list_view")
+#             user = authenticate(request, username=username, password=password)
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect("list_view")
