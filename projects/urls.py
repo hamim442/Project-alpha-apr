cd322bb5b292629bc272456bd6d43ab69267bece
Al Hamim <hamim442@icloud.com>
2024-05-31 10:35:23 -0700

    Feature 14 complete

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from projects.views import list_view, detail_view
+from projects.views import list_view, detail_view, create_project
 
 urlpatterns = [
     path("", list_view, name="list_projects"),
     path("<int:id>/", detail_view, name="show_project"),
+    path("create/", create_project, name="create_project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
 from tasks.models import Task
+from projects.forms import ProjectForm
 
 # Create your views here.
+
+
+@login_required
+def create_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("list_projects")
+
+    else:
+        form = ProjectForm()
+
+    context = {"create_form": form}
+    return render(request, "projects/create.html", context)
 
 
 @login_required
