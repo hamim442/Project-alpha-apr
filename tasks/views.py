ea2c38a498b0ff824a5a8d8986996aa8aa892b5c
Al Hamim <hamim442@icloud.com>
2024-05-31 14:29:38 -0700

    Final submission

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from tasks.forms import TaskForm
 from tasks.models import Task
-from projects.models import Project
-from django.contrib.auth.models import User
+from django.contrib.auth.models import 
 
 
 @login_required
